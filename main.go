da11f296e553eb8ddd5df976c003930aef3bf4e7
vicepalma <madahra@gmail.com>
2024-06-10 20:08:19 -0400

    uploading code

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"go-jwt/controllers"
+	"go-jwt/initializers"
+	"go-jwt/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+	initializers.SyncDatabase()
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
+	r.GET("/validate", middleware.RequireAuth, controllers.Validate)
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
